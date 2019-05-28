36325b0e7ebc9ba06e2729a0a8f89d83f7cf4d28
4某人 <594180536@qq.com>
2019-05-28 09:13:20 +0800

    first_commit

---
+++ src/main/java/producer/Producer.java
@@ -0,0 +1,26 @@
+package producer;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import util.ConnectionUtils;
+
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private static final String QUEUE_NAME = "queue_simple_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+//        channel.basicPublish(QUEUE_NAME,"/",false,false,null,"zzzzzzzzzzzz".getBytes());
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.basicPublish("",QUEUE_NAME,null,"zzzzzzzzzz".getBytes());
+        channel.close();
+        connection.close();
+
+    }
+
+}
